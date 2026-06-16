95534fceb8e3ef09d987e33d37ac47253723c227
467109-Nuttanan <467109@wsk.ac.th>
2026-06-16 02:58:10 +0000

    pieton03.py

---
+++ pyton03.py
@@ -0,0 +1,7 @@
+width = float(input("ใส่ความกว้าง: ")) #รับค่าความกว้างและความยาวจากผู้ใช้
+length = float(input("ใส่ความยาว: "))
+
+area = width * length # คำนวณพื้นที่รูปสี่เหลี่ยมมุมฉาก
+
+print("พื้นที่รูปสี่เหลี่ยมมุมฉาก =", area) # แสดงผล
+print("author: Nuttanan Dasri 6/9")
